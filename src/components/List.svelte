b21fe2648bb06f1b6bf7c8da7135879d745be78c
Sergey_Osipov <sibkron@mail.ru>
2022-06-22 07:21:08 +0700

    Svelte To-Do List finshed

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -3,8 +3,20 @@
   import { items } from "../store";
   import TodoApi from "../TodoApi";
   import Item from "./Item.svelte";
+  import NewItem from "./NewItem.svelte";
+  import { v4 as uuidv4 } from "uuid";
 
-  function handleNewItem(e) {}
+  function handleNewItem(e) {
+    $items = [
+      {
+        id: uuidv4(),
+        text: e.detail,
+        completed: false,
+      },
+      ...$items,
+    ];
+    TodoApi.save($items);
+  }
 
   function handleUpdate(e) {
     const index = $items.findIndex((item) => item.id === e.detail.id);
@@ -17,6 +29,17 @@
     TodoApi.save($items);
   }
 
+  let itemsSorted = [];
+
+  $: {
+    itemsSorted = [...$items];
+    itemsSorted.sort((a, b) => {
+      if (a.completed && b.completed) return 0;
+      if (a.completed) return 1;
+      if (b.completed) return -1;
+    });
+  }
+
   onMount(async () => {
     $items = await TodoApi.getAll();
   });
@@ -25,7 +48,8 @@
 <main />
 
 <div class="list">
-  {#each $items as item (item)}
+  <NewItem on:newitem={handleNewItem} />
+  {#each itemsSorted as item (item)}
     <Item {...item} on:update={handleUpdate} on:delete={handleDelete} />
   {:else}
     <p class="list-status">No Items Exist</p>
